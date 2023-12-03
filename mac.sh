ccb4851b8435c3a1f5abbeb828efc99ba57cfcdd
Afeef Azzam <afeef.az11@gmail.com>
2023-12-03 16:20:02 +0200

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -3,13 +3,10 @@ brew tap epk/epk
 brew install --cask font-sf-mono-nerd-font
 brew install starship
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ~/.zshrc ~/.zshrc.backup
+
 echo 'eval "$(starship init zsh)"
 export ZSH="$HOME/.oh-my-zsh"
-plugins=(
-git
-zsh-autosuggestions
-)
+plugins=(zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh' > ~/.zshrc
 
 echo 'add_newline = false
@@ -19,8 +16,7 @@ format = "$python$directory$character"
 success_symbol = "[➜](bold green) "
 error_symbol = "[✗](bold red) "
 
-[package]
-disabled = true
-
 [python]
 format="(\\\($virtualenv\\\) )"' > ~/.config/starship.toml
+
+source ~/.zshrc
