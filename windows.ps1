63d77f0119a10f49e8bb3bd8d2c9283d7f35cdf7
Afeef Azzam <afeef.az11@gmail.com>
2023-12-03 15:09:42 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,27 +1,13 @@
-Install-Module -Name PowerShellGet -Force
-Install-Module PSReadLine -AllowPrerelease -Force
 Install-Module PSReadLine
-
 winget install --id Starship.Starship
 
-$exp = "Import-Module PSReadLine
-
-Invoke-Expression (&starship init powershell)
-
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# Autocompleteion for Arrow keys
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-Set-PSReadLineOption -ShowToolTips
+$exp = "Invoke-Expression (&starship init powershell)
 Set-PSReadLineOption -PredictionSource History"
 
 $exp | Out-File ~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
 
-New-Item -ItemType Directory -Force ~/.config;New-Item -ItemType file -Force ~/.config/starship.toml;
+New-Item -ItemType Directory -Force ~/.config;
+New-Item -ItemType file -Force ~/.config/starship.toml;
 
 $starship = 'add_newline = false
 format = "$python$directory$character"
@@ -32,4 +18,4 @@ error_symbol = "[✗](bold red) "
 disabled = true
 [python]
 format="(($virtualenv) )"'
-$starship | Out-File ~/.config/starship.toml
+$starship | Out-File -Encoding utf8 ~/.config/starship.toml
