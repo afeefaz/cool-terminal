d7a9f5fc24cda46d639e13f7248c9e512b826972
Afeef Azzam <afeef.az11@gmail.com>
2023-12-04 23:41:11 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,21 +1,20 @@
 sudo apt install zsh -y
+curl -sS https://webi.sh/lsd | sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
-curl -sS https://starship.rs/install.sh | sh
+curl -s https://ohmyposh.dev/install.sh | bash -s
 
-echo 'eval "$(starship init zsh)"
-export ZSH="$HOME/.oh-my-zsh"
-plugins=(zsh-autosuggestions)' > ~/.zshrc
+echo 'export ZSH="$HOME/.oh-my-zsh"
+HISTFILE=$ZSH/.zsh_history
+plugins=(zsh-autosuggestions)
+export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
+source $ZSH/oh-my-zsh.sh
+autoload -U compinit && compinit -d $ZSH/cache/zcompdump
 
-echo 'add_newline = false
-format = "$python$directory$character"
-
-[character]
-success_symbol = "[➜](bold green) "
-error_symbol = "[✗](bold red) "
-
-[python]
-format="(\\\($virtualenv\\\) )"' > ~/.config/starship.toml
+#aliases
+alias ls="lsd"
+alias ll="lsd -lah"
+eval "$(oh-my-posh init zsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cert.omp.json)"' > ~/.zshrc
 
 source ~/.oh-my-zsh/oh-my-zsh.sh
 source ~/.zshrc
